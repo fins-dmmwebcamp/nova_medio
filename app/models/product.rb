39f1e63d71f3f4714655002d4b2ee09a1d2a11f3
naka1998 <bjtn1290411@gn.iwasaki.ac.jp>
2019-11-16 13:34:34 +0900

    create relations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+  has_many :products
+  has_many :order_details
+  has_many :favorites
+  has_many :cart_items
+  has_many :arrivals
+  has_many :discs
+  belongs_to :artist
+  belongs_to :label
+  belongs_to :genre
+  attachment :product_image
 end
