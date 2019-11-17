b9f4b9acfa4f8e6adca292296b7223455b8e234b
naka1998 <walkman0208@gmail.com>
2019-11-17 18:44:36 +0900

    作成　order(indexのみ)とarrivalのコントローラー

--- app/controllers/admin/arrivals_controller.rb
+++ app/controllers/admin/arrivals_controller.rb
@@ -1,10 +1,22 @@
 class Admin::ArrivalsController < ApplicationController
   def index
+    @arrivals = Arrivals.all
   end
 
   def new
+    @arrival = Arrivals.new
   end
 
   def create
+    @arrival = Arrivals.new(arrival_params)
+    if @arrival.save
+      redirect_to arrivals_path
+    else
+      render action: :new
+    end
+  end
+  private
+  def arrival_params
+    params.require(:arrival).permit(:amount, :product_id)
   end
 end
