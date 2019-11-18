d3027408a6e956b0f8e4ac8c1257e69b9f57a03a
naka1998 <bjtn1290411@gn.iwasaki.ac.jp>
2019-11-18 10:36:08 +0900

    追加　在庫の計算

--- app/controllers/admin/arrivals_controller.rb
+++ app/controllers/admin/arrivals_controller.rb
@@ -10,12 +10,17 @@ class Admin::ArrivalsController < ApplicationController
   def create
     @arrival = Arrivals.new(arrival_params)
     if @arrival.save
+      product = Products.find(@arrival.product_id)
+      product.stock += @arrival.amount
+      product.update
       redirect_to arrivals_path
     else
       render action: :new
     end
   end
+
   private
+
   def arrival_params
     params.require(:arrival).permit(:amount, :product_id)
   end
