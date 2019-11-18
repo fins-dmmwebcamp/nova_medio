1f0c29bf4c792e0c13bb3218547fa80d8e349cf5
飯田夕葵 <yukihamu1206@iidayuuaoinoMacBook.local>
2019-11-19 00:09:25 +0900

    signup途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+	  before_action :configure_permitted_parameters, if: :devise_controller?
+	  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name_first, :name_last, :name_first_kana, :name_last_kana, :postal_code, :address_prefecture,:address_city,:address_after,:phone_number,:email ,:characters, :passward])
+    end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,16 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+ 
+
+
+  has_many :destinations
+
+  accepts_nested_attributes_for :destinations
+
+
+
+
   has_many :orders
   has_many :reviews
   has_many :favorites
