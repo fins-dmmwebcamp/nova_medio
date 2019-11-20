ccd1bf5ced6ccdd67d14fbb758c73a33f5359e2c
fujino0878 <Kingdomdjdj@gmail.com>
2019-11-20 04:32:30 +0000

    アーティスト編集終わりました

--- app/controllers/admin/artists_controller.rb
+++ app/controllers/admin/artists_controller.rb
@@ -10,11 +10,23 @@ class Admin::ArtistsController < ApplicationController
   end
 
   def edit
+    @artist=Artist.find(params[:id])
   end
 
   def update
+    @artist=Artist.find(params[:id])
+    if @artist.update(artist_params)
+      redirect_to admin_artists_path
+    else
+      flash[:notice] = "error error not successfully edited!"
+      render :edit
+    end
   end
 
   def destroy
   end
+  private
+    def artist_params
+      params.require(:artist).permit(:name)
+    end
 end
