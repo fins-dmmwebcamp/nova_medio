3e42f0251ac825c90278cf7499060192ce719841
hikaru88 <hikaru8705@gmail.com>
2019-11-25 16:49:02 +0900

    uプロダクトソート途中

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class  ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products_new = Product.all.order(id: "DESC")
+    # @products_fovorite = Product.all
+    # @products_favorite = Product.joins(:favorites).group(:product_id).order('count(customer_id) desc')
+    @products_favorite = Product.all.joins(:favorites).group(:product_id).order('count(customer_id) desc')
+    @products_hot = Product.all.order(sales: "DESC")
   end
 
   def show
