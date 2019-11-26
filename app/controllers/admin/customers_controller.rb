d56b35701ea26281868413200434d904a6403134
yukihamu1206 <yukihamu1206@gmail.com>
2019-11-26 06:44:14 +0000

    ユーザー編集やばいです

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -15,32 +15,29 @@ class Admin::CustomersController < ApplicationController
 
   def edit
     @customer = Customer.find(params[:id])
-    @destination = Destination.find_by(customer_id: @customer.id)
+    @destination = ＠customer.where(is_main: true).find_by(customer_id: @customer.id)
   end
 
   def update
     @customer = Customer.find(params[:id])
-    @destination = Destination.find_by(customer_id: @customer.id)
+    @destination = Destination.where(is_main: true).find_by(customer_id: @customer.id)
 
     if @customer.update(customer_params) && @destination.update(customer_params)
     redirect_to admin_customer_path(params[:id])
     else
-      render :index
+      render :edit
     end
 
   end
 
   def destroy
-    customer = Customer.find(params[:id])
-    customer.is_deleted = true
-    customer.save
-    redirect_to  admin_customers_path
+   
   end
 
 
 
 def customer_prams
-  params.require(:customer).permit(:name_full,:phone_number,:email,destionations_attributes:[:postal_code,:address_prefecture,:address_city,:address_after])
+  params.require(:customer).permit(:name_full,:phone_number,:email,destionations_attributes:[:postal_code,:address_prefecture,:address_city,:address_after,:_destroy,:id])
 end
 
 
