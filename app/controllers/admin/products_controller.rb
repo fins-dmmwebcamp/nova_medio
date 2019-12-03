bad6fed9a4cf1e60a36b4c1a3c095fd4641a5538
hikaru88 <hikaru8705@gmail.com>
2019-12-03 16:09:18 +0900

    商品編集完成

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,9 +1,28 @@
 class Admin::ProductsController < ApplicationController
   def index
-    @products = Product.page(params[:page]).per(10)
+    # @products = Product.page(params[:page]).per(10)
+    if params[:key] == nil
+      @products = Product.all
+    elsif params[:key] == "new"
+      @products = Product.all.order(id: "DESC")
+    elsif params[:key] == "favorite"
+      @products = Product.all.joins(:favorites).group(:product_id).order('count(customer_id) desc')
+    elsif params[:key] == "hot"
+      @products = Prodct.all.order(sales: "DESC")
+    end
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def show
+    @product = Product.find(params[:id])
+    if @product.on_sale == true
+      @on_sale = "販売中"
+    else
+      @on_sale = "販売停止中"
+    end
   end
 
   def new
@@ -24,9 +43,13 @@ class Admin::ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to admin_product_path(@product)
   end
 
   def destroy
