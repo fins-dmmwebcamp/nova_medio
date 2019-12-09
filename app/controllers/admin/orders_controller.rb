337146f52e1dbfccaf0cea257d250734f5b6e424
hikaru88 <hikaru8705@gmail.com>
2019-12-09 16:17:37 +0900

    とりあえずorderコントローラーだいたい完成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,11 +5,31 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @price = total(@order.orders_details)
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_back(fallback_location: root_path)
   end
 
   def user_index
+    @customer = Customer.find_by(id: params[:customer_id])
   end
+
+  private
+
+    def order_params
+      params.require(:order).permit(:shipping_status)
+    end
+
+    def total(items)
+      total = 0
+      items.each do |item|
+        total += item.price * item.amount
+      end
+      total
+    end
+
 end
