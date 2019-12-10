775a026cec91590f46653eea07e16bb760c3174c
hikaru88 <hikaru8705@gmail.com>
2019-12-10 23:36:16 +0900

    バリデーション、エラーメッセージ、細かい修正

--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -1,3 +1,5 @@
 class Arrival < ApplicationRecord
   belongs_to :product
+  validates :amount, presence: true, numericality: { only_integer: true }
+  validates :product_id, presence: true
 end
--- app/models/disc.rb
+++ app/models/disc.rb
@@ -2,4 +2,5 @@ class Disc < ApplicationRecord
   belongs_to :product, inverse_of: :discs
   has_many :songs, dependent: :destroy, inverse_of: :disc
   accepts_nested_attributes_for :songs, allow_destroy: true
+  validates :order, presence: true, numericality: { only_integer: true }
 end
